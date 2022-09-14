385ed9517b28db752fc0305d04fa0e182e0e0538
cmike1775 <102187519+cmike1775@users.noreply.github.com>
2022-09-14 13:00:36 -0500

    Feature 16 Complete

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from tasks.models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.views.generic.list import ListView
 
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
@@ -10,3 +11,11 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
     template_name = "tasks/create.html"
     fields = ["name", "start_date", "due_date", "project", "assignee"]
     success_url = reverse_lazy("show_project")
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "tasks/mine.html"
+
+    def get_queryset(self):
+        return Task.objects.filter(assignee=self.request.user)
