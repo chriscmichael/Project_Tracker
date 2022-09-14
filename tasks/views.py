4facaf824af7ef1d022969a317e055271e7cfbd2
cmike1775 <102187519+cmike1775@users.noreply.github.com>
2022-09-14 12:47:57 -0500

    Feature 15 Complete, 1 error

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from tasks.models import Task
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = "tasks/create.html"
+    fields = ["name", "start_date", "due_date", "project", "assignee"]
+    success_url = reverse_lazy("show_project")
